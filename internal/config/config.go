93b0d70ead5bb8471a346abc9566e8cd6459b95b
Danindu de Silva <danindu001@gmail.com>
2026-03-15 10:54:02 +0000

    feat: add payment provider configs

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,18 +6,26 @@ import (
 )
 
 type Config struct {
-	AppEnv   string
-	HTTPPort string
+	AppEnv           string
+	HTTPPort         string
+	PaymentsProvider string
+	StripeSecretKey  string
 }
 
 func MustLoad() Config {
 	cfg := Config{
-		AppEnv:   getEnv("APP_ENV", "development"),
-		HTTPPort: getEnv("HTTP_PORT", "8080"),
+		AppEnv:           getEnv("APP_ENV", "development"),
+		HTTPPort:         getEnv("HTTP_PORT", "8080"),
+		PaymentsProvider: getEnv("PAYMENTS_PROVIDER", "fake"),
+		StripeSecretKey:  getEnv("STRIPE_SECRET_KEY", ""),
 	}
 
 	if cfg.HTTPPort == "" {
 		panic("HTTP_PORT must not be empty")
+	}
+
+	if cfg.PaymentsProvider == "" {
+		panic("PAYMENTS_PROVIDER must not be empty")
 	}
 
 	return cfg
